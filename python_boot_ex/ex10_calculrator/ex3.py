3a3f9cd9bcdbe01ec223a065b306cedcebdd089b
dev_leem <40571164+tavano2@users.noreply.github.com>
2023-12-04 14:49:14 +0900

    day 10 일부 완료
    day 10 일부 완료

---
+++ python_boot_ex/ex10_calculrator/ex3.py
@@ -0,0 +1,28 @@
+# code Challenges
+def is_leap(year):
+    chk_leap = False
+    if year % 4 == 0:
+        if year % 100 == 0:
+            if year % 400 == 0:
+                chk_leap = True
+                return chk_leap
+            else:
+                return chk_leap
+        else:
+            chk_leap = True
+            return chk_leap
+    else:
+        return chk_leap
+
+
+def days_in_month(year, month):
+    month_days = [31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31]
+    if is_leap(year) and month == 2:
+        return 29
+    return month_days[month - 1]
+
+
+year = int(input())
+month = int(input())
+days = days_in_month(year, month)
+print(days)
